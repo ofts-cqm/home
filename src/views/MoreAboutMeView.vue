1a9bf3c015b9144a2d943668fce672aef1a89d56
ofts-cqm <qianmuchen.sam@gmail.com>
2026-07-11 17:47:33 -0400

    feat: add AI story

--- src/views/MoreAboutMeView.vue
+++ src/views/MoreAboutMeView.vue
@@ -1,5 +1,22 @@
 <template>
   <main>
+    <h1>What is this?</h1>
+
+    <div class="paragraph">
+        <p>
+            These are a bit of my personal stories, if you are interested. If you are an HR looking for my projects and 
+            experiences, then you are probably in the wrong file. <span class="wip">Open the terminal and type 
+            <span class="command">find ./ -name "MoreAboutMe*"</span>, this is the file you are looking for.</span>
+            (Terminal is still WIP)
+        </p>
+
+        <p>
+            However, if you just want to learn more about me, you are welcome to stay. 
+            Nothing here is technical, nor are they related to any of my skills. This file is more like a tree hole. 
+            I want to write something about my true thoughts, and hope someone will resonate with me. 
+        </p>
+    </div>
+
     <h1>So, Why Software Engineering?</h1>
 
     <div class="paragraph">
@@ -52,19 +69,69 @@
         </p>
     </div>
 
+    <h1>What do you think about AI?</h1>
+
+    <div class="paragraph">
+        <p>
+            AI is the greatest tool ever invented, with the help of AI I gain unimaginable productivity. 
+            I have to be honest, without AI, I lost most of the efficiency, but with the help of AI, I can 
+            vibe-code or semi-vibe-code comprehensive systems and softwares. 
+        </p>
+
+        <h2>Do you like to use AI?</h2>
+
+        <p>
+            Actually, No... which may suprise you. This is my honest answer. You see, I study software engineering because coding is an
+            enjoyable thing. If its not enjoyable why would I choose this major? But, to me, the process of coding is 
+            what is enjoyable. The outcome, the result... not really important. Coding, debugging, 
+            sitting in front of the computer wondering why this doesn't work, and sitting in front of the computer
+            celebrating it finally works, are all process of coding, which I consider to be enjoyable, 
+            even debugging for hours. 
+        </p>
+
+        <p>
+            As you can see, AI destroys this feeling. The entire enjoyable process is replaced. I don't care about 
+            the result. If I made a project, I get a sense of pride and a sense of achievement. If I let AI do it, 
+            I get nothing, because this is literally other people's work. Sometimes I struggle about this. 
+            If I don't use AI then I lost productivity. If I use AI then I lost fun. 
+        </p>
+
+            
+
+        <h2>
+            Is this website made by AI?
+        </h2>   
+
+        <p>Nope, not even a single line. AI is for efficiency and productivity, this site is for fun. </p> 
+
+        <h2>
+            Then, are you skill OK with using AI in work?
+        </h2>
+
+        <p>If AI is required to boost productivity, then definately. This is my work, and I have the 
+            responsibility to deliver my content on time with high quality. I am not an AI hater, nor an AI fun, 
+            it is just a tool that is unfortunately not my top choice. I still use AI, and know very well how to use AI. 
+        </p>
+
+        <p>
+            It is the future of work, the future of software engineering. I am still very agile, able to adopt to new technology. 
+        </p>
+    </div>
+
     <h1>How Do You Feel About Studying Software Engineering?</h1>
 
     <div class="paragraph">
-        <h2>Here is a meme from reddit:</h2>
+        <h2 class="meme_title">Here is a meme from reddit:</h2>
         <img class="meme" src="/src/assets/meme.png"/>
     </div>
+
   </main>
 </template>
 
 <style scoped src="./ViewCommon.css"></style>
 
-<style>
-h2{
+<style scoped>
+.meme_title{
     padding: 2rem 0 2rem 0;
 }
 
@@ -74,6 +141,17 @@ h2{
     width: auto !important;
     max-width: 90%;
     margin-right: auto;
+}
+
+.command{
+    background-color: dimgrey;
+    border-radius: 0.3rem;
+    padding-left: 0.3rem;
+    padding-right: 0.3rem;
+}
+
+.wip{
+    text-decoration: line-through;
 }
 
 </style>
